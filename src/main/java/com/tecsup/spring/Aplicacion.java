8abba17af77e81e0adc8e353e24b11f2886f47c7
alumno <alumno@192.168.19.56>
2015-06-28 10:41:20 -0500

    agregar un metodo mensaje

--- src/main/java/com/tecsup/spring/Aplicacion.java
+++ src/main/java/com/tecsup/spring/Aplicacion.java
@@ -10,10 +10,19 @@ package com.tecsup.spring;
  */
 public class Aplicacion {
     
+    
+    
     public static void main(String[] args) {
         
         System.out.println("tecsup");
         
+    }
+    
+    
+    
+    public static void mensaje ()
+    {
+        System.out.println("este es un cambio");
     }
     
 }
